d754ff95235746e77738d69a30a271d60d8e347d
vk0413hwq <963561754@qq.com>
2020-07-09 09:51:31 +0800

    1
    1

--- src/js/details.js
+++ src/js/details.js
@@ -1,7 +1,14 @@
 $(function(){
     // 放大镜
-
-
+    // 获取大中小盒子 
+    var $cover = $('.cover');
+    var $lgbox = $('.lg_show')
+    var $midbox = $('.details_all_lg_img');
+    var $fdlist = $('.lb_container').find('img');
+    console.log(11111)
+    $fdlist.each(function (index) {
+            console.log($fdlist(index))
+      })
 
 
     // 渲染页面
@@ -17,30 +24,32 @@ $(function(){
         // ${pid} ${pname} ${pimg} ${pprice} ${pnum}
         $('.details_price01').html(p_price);
         $('.details_name01').html(p_name); 
-    })
 
-
-    // 详情页添加商品
-    var $btn_addcart = $('.join-sc');
-    var $num = $('#pro_num');
-    $btn_addcart.click(function(){
-    console.log($num.val())
-        $.get('./../php/addwq.php?id=1&name=牛油果&img=./../images/nyg (1).jpg&price=25',function(data){
-            var json = JSON.parse(data);
-            if(json.code){
-                // 如果添加成功弹出隐藏底幕
-                $('.shop_hid').css({
-                    'display':'block'
-                })
-                $('.sh_lt').click(function(){
-                    location.href = './details.html'
-                })
-                $('.sh_gt').click(function(){
-                    location.href = './shopcart.html'
-                })
-            }
+        // 详情页添加商品
+        var $btn_addcart = $('.join-sc');
+        var $num = $('#pro_num');
+        $btn_addcart.click(function(){
+        console.log($num.val())
+            $.get('./../php/addwq.php?id=1&name=牛油果&img=./../images/nyg (1).jpg&price=25',function(data){
+                var json = JSON.parse(data);
+                if(json.code){
+                    // 如果添加成功弹出隐藏底幕
+                    $('.shop_hid').css({
+                        'display':'block'
+                    })
+                    $('.sh_lt').click(function(){
+                        location.href = './details.html'
+                    })
+                    $('.sh_gt').click(function(){
+                        location.href = './shopcart.html'
+                    })
+                }
+            })
         })
     })
+
+
+    
 
 
 
